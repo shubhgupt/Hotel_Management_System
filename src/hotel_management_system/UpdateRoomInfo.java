300ffa79f6f4b074126b310bfcefcbc8cd9a430e
Shubham Gupta <shubhgupt932@gmail.com>
2021-07-22 12:17:40 +0530

    Now I have added the Availability, Status and Price Field too and price field will only get edited if admin enters his password

--- src/hotel_management_system/UpdateRoomInfo.java
+++ src/hotel_management_system/UpdateRoomInfo.java
@@ -7,21 +7,25 @@ package hotel_management_system;
 import java.awt.Choice;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.ResultSet;
 import javax.swing.*;
 
 public class UpdateRoomInfo extends JFrame{
     
-    Choice c1,c2,c3,c4;
-    Conn c;
-    String roomQuery1 = "SELECT roomnumber from rooms where roomnumber Not In (Select roomNumber from customers)";
-    String roomQuery2 = "Select RoomNumber from customers where customerId =";
+    private Choice c1,c2,c3,c4;
+    private Conn c;
+    private JTextField t1;
+    private String roomQuery1 = "SELECT roomnumber from rooms where roomnumber Not In (Select roomNumber from customers)";
+    private String roomQuery2 = "Select RoomNumber from customers where customerId =";
     
     UpdateRoomInfo(){
         JPanel p1 = new JPanel();
-        p1.setBounds(0,50, 200, 200);
+        p1.setBounds(0,50, 200, 250);
         p1.setLayout(null);
         p1.setBackground(Color.white);
         add(p1);
@@ -46,11 +50,41 @@ public class UpdateRoomInfo extends JFrame{
         status.setFont(new Font("Tahoma", Font.PLAIN, 17));
         p1.add(status);
         
+        JLabel price = new  JLabel("Price");
+        price.setBounds(50, 200, 100, 25);
+        price.setFont(new Font("Tahoma", Font.PLAIN, 17));
+        p1.add(price);
+        
         JPanel p2 = new JPanel();
-        p2.setBounds(200,50, 180, 200);
+        p2.setBounds(200,50, 180, 250);
         p2.setLayout(null);
         p2.setBackground(Color.white);
         add(p2);
+        
+        t1 = new JTextField();
+        t1.setBounds(0, 200, 180, 25);
+        t1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        t1.setBackground(Color.white);
+        t1.setEditable(false);
+        p2.add(t1);
+        
+        c4 = new Choice();
+        c4.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        c4.setBackground(Color.white);
+        c4.setBounds(0, 150, 180, 25);
+        c4.setFocusable(false);
+        c4.add("Cleaned");
+        c4.add("Uncleaned");
+        p2.add(c4);
+        
+        c3 = new Choice();
+        c3.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        c3.setBackground(Color.white);
+        c3.setBounds(0, 100, 180, 25);
+        c3.setFocusable(false);
+        c3.add("Available");
+        c3.add("Occupied");
+        p2.add(c3);
         
         c2 = new Choice();
         c2.setFont(new Font("Tahoma", Font.PLAIN, 14));
@@ -60,18 +94,11 @@ public class UpdateRoomInfo extends JFrame{
         p2.add(c2);
         c2.addItemListener(new ItemListener(){
             public void itemStateChanged(ItemEvent e){
-                
+                int roomNumber = Integer.parseInt((String) e.getItem()) ;
+                selectElementFromC3C4AndT1(roomNumber);
             }
         });
         
-        c3 = new Choice();
-        c3.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        c3.setBackground(Color.white);
-        c3.setBounds(0, 50, 180, 25);
-        c3.setFocusable(false);
-        c3.add("Available");
-        c3.add("Occupied");
-        p2.add(c3);
         
         c1 = new Choice();
         c1.setFont(new Font("Tahoma", Font.PLAIN, 14));
@@ -92,8 +119,20 @@ public class UpdateRoomInfo extends JFrame{
             }
         });
         
-        
-        
+        ImageIcon editable = new ImageIcon(ClassLoader.getSystemResource("hotel_management_system/icons/edit.png"));
+        Image edit = editable.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
+        JLabel editIcon = new JLabel(new ImageIcon(edit));
+        editIcon.setBounds(173, 200, 25, 25);
+        p1.add(editIcon);
+        editIcon.addMouseListener(new MouseAdapter(){
+            public void mousePressed(MouseEvent evt){
+                if(c1.getSelectedItem().equals("Update Rooms Only")){
+                    String password = JOptionPane.showInputDialog(rootPane, "Enter Admin Password!", "Confirm Admin", HEIGHT);
+                    if(password != null)
+                        validatePassword(password);
+                }
+            }
+        });
         
 //        setUndecorated(true);
         getContentPane().setBackground(Color.white);
@@ -103,13 +142,55 @@ public class UpdateRoomInfo extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
+    private void validatePassword(String password){
+        try{
+         
+            c = new Conn();
+            String query = "Select * from login where password ='" + password+"'";
+            ResultSet rs = c.st.executeQuery(query);
+            if(rs.next()){
+                t1.setEditable(true);
+            }else{
+                JOptionPane.showMessageDialog(rootPane,"Wrong Password!", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+            
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(rootPane,"Technical Issue!", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+    private void selectElementFromC3C4AndT1(int roomNumber){
+        try{
+            
+            c = new Conn();
+            String query = "Select availability, status, price from rooms where roomNumber ="+roomNumber;
+            ResultSet rs = c.st.executeQuery(query);
+            rs.next();
+            c3.select(rs.getString(1));
+            c4.select(rs.getString(2));
+            t1.setText(""+rs.getDouble(3));
+            
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(rootPane,"Technical Issue!", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
     private void addElementsToC2(String query){
         
         try{
             c = new Conn();
             ResultSet rs = c.st.executeQuery(query);
             c2.removeAll();
+            boolean flag = true;
             while(rs.next()){
+                if(flag){
+                    selectElementFromC3C4AndT1(rs.getInt(1));
+                    flag = false;
+                }
                 c2.add(""+rs.getInt(1));
             }
             
